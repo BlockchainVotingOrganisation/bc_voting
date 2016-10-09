3d20b3aeacc4b8050f9753aa4987f763bb3466f2
luzi41 <com@goettertz.de>
2016-10-09 09:29:22 +0200

    Add files via upload

---
+++ ext_localconf.php
@@ -0,0 +1,65 @@
+<?php
+// Rev. 93
+if (!defined('TYPO3_MODE')) {
+	die('Access denied.');
+}
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+	'Goettertz.' . $_EXTKEY,
+	'Project',
+	array(
+ 		'Project' => 'list, show, new, create, edit, editbcparams, update, delete, assign, evaluation, arguments, settings, createSettings, removeLogo, checkVoting, execute',
+ 		'Option' => 'list, show, new, create, edit, update, delete, vote, removeLogo',
+ 		'Wallet' => 'show, sendAssets',
+ 		'User' => 'list, show, new, create, edit, update, delete, import',
+		'Blockchain' => 'show',
+		'Argument' => 'list, show, new, create, edit, update, delete',
+		'Ballot' => 'list, show, new, create, edit, update, delete, sealBallot, vote, removeLogo',
+	),
+	// non-cacheable actions
+	array(
+		'Project' => 'list, show, new, create, edit, editbcparams, update, delete, assign, evaluation, arguments, settings, createSettings, removeLogo, checkVoting, execute',
+		'Option' => 'list, show, new, create, edit, update, delete, vote, removeLogo',
+		'Wallet' => 'show, sendAssets',
+ 		'User' => 'list, show, new, create, edit, update, delete, import',
+		'Blockchain' => 'show',
+		'Argument' => 'list, show, new, create, edit, update, delete',
+		'Ballot' => 'list, show, new, create, edit, update, delete, sealBallot, vote, removeLogo',
+	)
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter('Goettertz\\BcVoting\\Property\\TypeConverter\\UploadedFileReferenceConverter');
+// \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter('Goettertz\\BcVoting\\Property\\TypeConverter\\ObjectStorageConverter');
+		
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+				'Goettertz.' . $_EXTKEY,
+				'Bvs_nav1',
+				array('Project' => 'nav1'),
+				array('Project' => 'nav1')
+);
+
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+		'Goettertz.' . $_EXTKEY,
+		'BVS_newProjectWizard',
+		array(
+				'Project' => 'new, create, createSettings, update'
+		),
+		array(
+				'Project' => 'new, create, createSettings, update'
+		)
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+		'Goettertz.' . $_EXTKEY,
+		'BVS_personalWallet',
+		array(
+				
+				'Wallet' => 'show, send'
+		),
+		array(
+				
+				'Wallet' => 'show, send'
+		)
+);
+?>
---
+++ ext_tables.php
@@ -0,0 +1,353 @@
+<?php
+/**
+ * Rev.76
+ */
+if (!defined('TYPO3_MODE')) {
+	die('Access denied.');
+}
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	$_EXTKEY,
+	'Project',
+	'BVS-Project'
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+		$_EXTKEY,
+		'Bvs_nav1',
+		'BVS-Navigation'
+		);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+		$_EXTKEY,
+		'BVS_newProjectWizard',
+		'BVS New Project Wizard'
+		);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+		$_EXTKEY,
+		'BVS_personalWallet',
+		'BVS Personal Wallet'
+		);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'bc_voting');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_bcvoting_domain_model_project', 'EXT:bc_voting/Resources/Private/Language/locallang_csh_tx_bcvoting_domain_model_project.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_bcvoting_domain_model_project');
+$GLOBALS['TCA']['tx_bcvoting_domain_model_project'] = array(
+	'ctrl' => array(
+		'title'	=> 'LLL:EXT:bc_voting/Resources/Private/Language/locallang_db.xlf:tx_bcvoting_domain_model_project',
+		'label' => 'name',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'dividers2tabs' => TRUE,
+
+		'versioningWS' => 2,
+		'versioning_followPages' => TRUE,
+
+		'languageField' => 'sys_language_uid',
+		'transOrigPointerField' => 'l10n_parent',
+		'transOrigDiffSourceField' => 'l10n_diffsource',
+		'delete' => 'deleted',
+		'enablecolumns' => array(
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
+			'endtime' => 'endtime',
+		),
+		'searchFields' => 'name,description,start,end,assignments,',
+		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Project.php',
+		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_bcvoting_domain_model_project.gif'
+	),
+);
+
+# Asset
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_bcvoting_domain_model_asset', 'EXT:bc_voting/Resources/Private/Language/locallang_csh_tx_bcvoting_domain_model_asset.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_bcvoting_domain_model_asset');
+$GLOBALS['TCA']['tx_bcvoting_domain_model_ballot'] = array(
+		'ctrl' => array(
+				'title'	=> 'LLL:EXT:bc_voting/Resources/Private/Language/locallang_db.xlf:tx_bcvoting_domain_model_asset',
+				'label' => 'name',
+				'tstamp' => 'tstamp',
+				'crdate' => 'crdate',
+				'cruser_id' => 'cruser_id',
+				'dividers2tabs' => TRUE,
+
+				'versioningWS' => 2,
+				'versioning_followPages' => TRUE,
+
+				'languageField' => 'sys_language_uid',
+				'transOrigPointerField' => 'l10n_parent',
+				'transOrigDiffSourceField' => 'l10n_diffsource',
+				'delete' => 'deleted',
+				'enablecolumns' => array(
+						'disabled' => 'hidden'
+				),
+				'searchFields' => 'name',
+				'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Asset.php',
+				'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_bcvoting_domain_model_project.gif'
+		),
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_bcvoting_domain_model_project', 'EXT:bc_voting/Resources/Private/Language/locallang_csh_tx_bcvoting_domain_model_ballot.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_bcvoting_domain_model_ballot');
+$GLOBALS['TCA']['tx_bcvoting_domain_model_ballot'] = array(
+		'ctrl' => array(
+				'title'	=> 'LLL:EXT:bc_voting/Resources/Private/Language/locallang_db.xlf:tx_bcvoting_domain_model_ballot',
+				'label' => 'name',
+				'tstamp' => 'tstamp',
+				'crdate' => 'crdate',
+				'cruser_id' => 'cruser_id',
+				'dividers2tabs' => TRUE,
+
+				'versioningWS' => 2,
+				'versioning_followPages' => TRUE,
+
+				'languageField' => 'sys_language_uid',
+				'transOrigPointerField' => 'l10n_parent',
+				'transOrigDiffSourceField' => 'l10n_diffsource',
+				'delete' => 'deleted',
+				'enablecolumns' => array(
+						'disabled' => 'hidden',
+						'starttime' => 'starttime',
+						'endtime' => 'endtime',
+				),
+				'searchFields' => 'text,footer,project',
+				'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Ballot.php',
+				'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_bcvoting_domain_model_project.gif'
+		),
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_bcvoting_domain_model_option', 'EXT:bc_voting/Resources/Private/Language/locallang_csh_tx_bcvoting_domain_model_option.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_bcvoting_domain_model_option');
+$GLOBALS['TCA']['tx_bcvoting_domain_model_option'] = array(
+	'ctrl' => array(
+		'title'	=> 'LLL:EXT:bc_voting/Resources/Private/Language/locallang_db.xlf:tx_bcvoting_domain_model_option',
+		'label' => 'name',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'dividers2tabs' => TRUE,
+
+		'versioningWS' => 2,
+		'versioning_followPages' => TRUE,
+
+		'languageField' => 'sys_language_uid',
+		'transOrigPointerField' => 'l10n_parent',
+		'transOrigDiffSourceField' => 'l10n_diffsource',
+		'delete' => 'deleted',
+		'enablecolumns' => array(
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
+			'endtime' => 'endtime',
+		),
+		'searchFields' => 'name,wallet_address,ballot,color',
+		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Option.php',
+		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_bcvoting_domain_model_option.gif'
+	),
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_bcvoting_domain_model_argument', 'EXT:bc_voting/Resources/Private/Language/locallang_csh_tx_bcvoting_domain_model_argument.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_bcvoting_domain_model_argument');
+$GLOBALS['TCA']['tx_bcvoting_domain_model_argument'] = array(
+		'ctrl' => array(
+				'title'	=> 'LLL:EXT:bc_voting/Resources/Private/Language/locallang_db.xlf:tx_bcvoting_domain_model_argument',
+				'label' => 'name',
+				'tstamp' => 'tstamp',
+				'crdate' => 'crdate',
+				'cruser_id' => 'cruser_id',
+				'dividers2tabs' => TRUE,
+
+				'versioningWS' => 2,
+				'versioning_followPages' => TRUE,
+
+				'languageField' => 'sys_language_uid',
+				'transOrigPointerField' => 'l10n_parent',
+				'transOrigDiffSourceField' => 'l10n_diffsource',
+				'delete' => 'deleted',
+				'enablecolumns' => array(
+						'disabled' => 'hidden',
+						'starttime' => 'starttime',
+						'endtime' => 'endtime',
+				),
+				'searchFields' => 'name,text,option,project,',
+				'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Argument.php',
+				'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_bcvoting_domain_model_option.gif'
+		),
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_bcvoting_domain_model_voting', 'EXT:bc_voting/Resources/Private/Language/locallang_csh_tx_bcvoting_domain_model_voting.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_bcvoting_domain_model_voting');
+$GLOBALS['TCA']['tx_bcvoting_domain_model_voting'] = array(
+		'ctrl' => array(
+				'title'	=> 'LLL:EXT:bc_voting/Resources/Private/Language/locallang_db.xlf:tx_bcvoting_domain_model_voting',
+				'label' => 'reference',
+				'tstamp' => 'tstamp',
+				'crdate' => 'crdate',
+				'cruser_id' => 'cruser_id',
+				'versioningWS' => 2,
+				'languageField' => 'sys_language_uid',
+				'transOrigPointerField' => 'l10n_parent',
+				'transOrigDiffSourceField' => 'l10n_diffsource',
+				'delete' => 'deleted',
+				'enablecolumns' => array(
+					'disabled' => 'hidden',
+					'starttime' => 'starttime',
+					'endtime' => 'endtime',
+				),
+				'searchFields' => 'reference,txid,secret',
+				'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Voting.php',
+				'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_bcvoting_domain_model_voting.gif'
+		),
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_bcvoting_domain_model_category', 'EXT:bc_voting/Resources/Private/Language/locallang_csh_tx_bcvoting_domain_model_category.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_bcvoting_domain_model_category');
+$GLOBALS['TCA']['tx_bcvoting_domain_model_category'] = array(
+		'ctrl' => array(
+				'title'	=> 'LLL:EXT:bc_voting/Resources/Private/Language/locallang_db.xlf:tx_bcvoting_domain_model_category',
+				'label' => 'name',
+				'tstamp' => 'tstamp',
+				'crdate' => 'crdate',
+				'cruser_id' => 'cruser_id',
+				'versioningWS' => 2,
+				'languageField' => 'sys_language_uid',
+				'transOrigPointerField' => 'l10n_parent',
+				'transOrigDiffSourceField' => 'l10n_diffsource',
+				'delete' => 'deleted',
+				'enablecolumns' => array(
+						'disabled' => 'hidden',
+						'starttime' => 'starttime',
+						'endtime' => 'endtime',
+				),
+				'searchFields' => 'name',
+				'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Category.php',
+				'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_bcvoting_domain_model_category.gif'
+		),
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_bcvoting_domain_model_assignment', 'EXT:bc_voting/Resources/Private/Language/locallang_csh_tx_bcvoting_domain_model_assignment.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_bcvoting_domain_model_assignment');
+$GLOBALS['TCA']['tx_bcvoting_domain_model_assignment'] = array(
+	'ctrl' => array(
+		'title'	=> 'LLL:EXT:bc_voting/Resources/Private/Language/locallang_db.xlf:tx_bcvoting_domain_model_assignment',
+		'label' => 'role',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'dividers2tabs' => TRUE,
+
+		'versioningWS' => 2,
+		'versioning_followPages' => TRUE,
+
+		'languageField' => 'sys_language_uid',
+		'transOrigPointerField' => 'l10n_parent',
+		'transOrigDiffSourceField' => 'l10n_diffsource',
+		'delete' => 'deleted',
+		'enablecolumns' => array(
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
+			'endtime' => 'endtime',
+		),
+		'searchFields' => 'project,role,user,',
+		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Assignment.php',
+		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_bcvoting_domain_model_assignment.gif'
+	),
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_bcvoting_domain_model_role', 'EXT:bc_voting/Resources/Private/Language/locallang_csh_tx_bcvoting_domain_model_role.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_bcvoting_domain_model_role');
+$GLOBALS['TCA']['tx_bcvoting_domain_model_role'] = array(
+	'ctrl' => array(
+		'title'	=> 'LLL:EXT:bc_voting/Resources/Private/Language/locallang_db.xlf:tx_bcvoting_domain_model_role',
+		'label' => 'name',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'dividers2tabs' => TRUE,
+
+		'versioningWS' => 2,
+		'versioning_followPages' => TRUE,
+
+		'languageField' => 'sys_language_uid',
+		'transOrigPointerField' => 'l10n_parent',
+		'transOrigDiffSourceField' => 'l10n_diffsource',
+		'delete' => 'deleted',
+		'enablecolumns' => array(
+			'disabled' => 'hidden',
+
+		),
+		'searchFields' => 'name,description,',
+		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Role.php',
+		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_bcvoting_domain_model_role.gif'
+	),
+);
+
+$GLOBALS['TCA']['tx_bcvoting_domain_model_element'] = array(
+		'ctrl' => array(
+				'title'	=> 'LLL:EXT:bc_voting/Resources/Private/Language/locallang_db.xlf:tx_bcvoting_domain_model_element',
+				'label' => 'property',
+				'tstamp' => 'tstamp',
+				'crdate' => 'crdate',
+				'cruser_id' => 'cruser_id',
+				'dividers2tabs' => TRUE,		
+				'versioningWS' => 2,
+				'versioning_followPages' => TRUE,
+				
+				'languageField' => 'sys_language_uid',
+				'transOrigPointerField' => 'l10n_parent',
+				'transOrigDiffSourceField' => 'l10n_diffsource',
+				'delete' => 'deleted',
+				'enablecolumns' => array(
+						'disabled' => 'hidden',
+				
+				),
+		),
+		'searchFields' => 'property,description,project',
+		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TCA/Element.php',
+		'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_bcvoting_domain_model_role.gif'
+);
+
+if (!isset($GLOBALS['TCA']['fe_users']['ctrl']['type'])) {
+	if (file_exists($GLOBALS['TCA']['fe_users']['ctrl']['dynamicConfigFile'])) {
+		require_once($GLOBALS['TCA']['fe_users']['ctrl']['dynamicConfigFile']);
+	}
+	// no type field defined, so we define it here. This will only happen the first time the extension is installed!!
+	$GLOBALS['TCA']['fe_users']['ctrl']['type'] = 'tx_extbase_type';
+	$tempColumns = array();
+	$tempColumns[$GLOBALS['TCA']['fe_users']['ctrl']['type']] = array(
+		'exclude' => 1,
+		'label'   => 'LLL:EXT:bc_voting/Resources/Private/Language/locallang_db.xlf:tx_bcvoting.tx_extbase_type',
+		'config' => array(
+			'type' => 'select',
+			'items' => array(),
+			'size' => 1,
+			'maxitems' => 1,
+		)
+	);
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('fe_users', $tempColumns, 1);
+}
+
+$GLOBALS['TCA']['fe_users']['types']['Tx_BcVoting_User']['showitem'] = $TCA['fe_users']['types']['0']['showitem'];
+$GLOBALS['TCA']['fe_users']['types']['Tx_BcVoting_User']['showitem'] .= ',--div--;LLL:EXT:bc_voting/Resources/Private/Language/locallang_db.xlf:tx_bcvoting_domain_model_user,';
+$GLOBALS['TCA']['fe_users']['types']['Tx_BcVoting_User']['showitem'] .= '';
+
+$GLOBALS['TCA']['fe_users']['columns'][$TCA['fe_users']['ctrl']['type']]['config']['items'][] = array('LLL:EXT:bc_voting/Resources/Private/Language/locallang_db.xlf:fe_users.tx_extbase_type.Tx_BcVoting_User','Tx_BcVoting_User');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('fe_users', $GLOBALS['TCA']['fe_users']['ctrl']['type'],'','after:' . $TCA['fe_users']['ctrl']['label']);
+
+$extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY));
+$pluginName = strtolower('Project');
+$pluginSignature = $extensionName.'_'.$pluginName;
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,list';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY . '/Configuration/FlexForms/ProjectConfiguration.xml');
+
+$extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY));
+$pluginName = strtolower('BVS_newProjectWizard');
+$pluginSignature = $extensionName.'_'.$pluginName;
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,list';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY . '/Configuration/FlexForms/ProjectWizardConfiguration.xml');
+
+?>
