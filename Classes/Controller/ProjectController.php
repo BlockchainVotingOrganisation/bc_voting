bcf247d9d808ed01cc6d8541084b0671361a15b1
luzi41 <com@goettertz.de>
2016-12-05 10:32:24 +0100

    Bugfixes 	modified:   Classes/Controller/ProjectController.php 	modified:   Classes/Domain/Model/Project.php

--- Classes/Controller/ProjectController.php
+++ Classes/Controller/ProjectController.php
@@ -1,7 +1,7 @@
 <?php
 namespace Goettertz\BcVoting\Controller;
 
-// ini_set("display_errors", 1);
+ini_set("display_errors", 1);
 
 /***************************************************************
  *
