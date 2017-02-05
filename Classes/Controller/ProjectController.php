b5edc0d2ce7e5fdb2d653f6ad95f105ab3c4e1c8
Louis Göttertz <com@goettertz.de>
2017-02-05 13:00:02 +0100

    Update ProjectController.php

--- Classes/Controller/ProjectController.php
+++ Classes/Controller/ProjectController.php
@@ -1,7 +1,7 @@
 <?php
 namespace Goettertz\BcVoting\Controller;
-//error_reporting(E_ALL);
-ini_set("display_errors", 1);
+// error_reporting(E_ALL);
+// ini_set("display_errors", 1);
 
 /***************************************************************
  *
